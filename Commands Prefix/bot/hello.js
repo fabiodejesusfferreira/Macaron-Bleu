a724a9a8fcef7d7ec2a2379f0c045b194cc45212
Fábio de Jesus <142427321+fabiodejesusfferreira@users.noreply.github.com>
2024-07-30 16:15:47 -0300

    Update hello.js
    This command is more to be used as a test, soon I'll put it in gitignore as well

--- Commands Prefix/bot/hello.js
+++ Commands Prefix/bot/hello.js
@@ -4,12 +4,15 @@ module.exports = {
     name: "hello",
     aliases: ["hi"],
 
-    run: async(client, msg, args) => {
+    run: async (client, msg, args) => {
         /**
          * @type {Message}
          */
         let message = msg
+        const url = message.member.displayAvatarURL({ size: 128, extension: 'png', forceStatic: false })
+        const regex = /\/avatars\/\d+\/([a-fA-F0-9]+)/;
+        const id = url.match(regex)[1];
 
-        message.reply('Hello World!')
+        message.reply(`Hello World!. Id do avatar: ${id}`)
     }
 }
