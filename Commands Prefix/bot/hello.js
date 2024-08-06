cb2962b4058efe0d893d510a2f9d3feac066dcd1
Fábio de Jesus <142427321+fabiodejesusfferreira@users.noreply.github.com>
2024-08-05 21:55:36 -0300

    Update hello.js
    Temporary change to see which servers the bot is on

--- Commands Prefix/bot/hello.js
+++ Commands Prefix/bot/hello.js
@@ -1,18 +1,18 @@
 const { EmbedBuilder, Message } = require("discord.js")
 
 module.exports = {
-    name: "hello",
-    aliases: ["hi"],
+    name: "command",
+    aliases: ["c"],
 
     run: async (client, msg, args) => {
         /**
          * @type {Message}
          */
         let message = msg
-        const url = message.member.displayAvatarURL({ size: 128, extension: 'png', forceStatic: false })
-        const regex = /\/avatars\/\d+\/([a-fA-F0-9]+)/;
-        const id = url.match(regex)[1];
 
-        message.reply(`Hello World!. Id do avatar: ${id}`)
+        if(message.author.id !== '1266403609487212705') return;
+
+        await message.delete();
+        message.channel.send(`<:discotoolsxyzicon70:1267636769688649872> | Comando de ${args[0]} feito`)
     }
 }
