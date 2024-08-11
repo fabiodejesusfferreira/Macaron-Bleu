a79f06a9621525d425e95b30c9eed541a2bbc1b5
Fábio de Jesus <142427321+fabiodejesusfferreira@users.noreply.github.com>
2024-08-11 11:38:05 -0300

    Update bot-info.js
    I've added more information about the bot, such as Discord.js version, node; ping, etc.

--- Commands Prefix/bot/bot-info.js
+++ Commands Prefix/bot/bot-info.js
@@ -1,4 +1,7 @@
-const { EmbedBuilder, Message } = require("discord.js")
+const { EmbedBuilder, Message, version, ActionRowBuilder, ButtonBuilder, ButtonStyle } = require("discord.js");
+const MongoClient = require("mongodb").MongoClient;
+const config = require('../../config.json');
+const process = require('node:process')
 
 module.exports = {
     name: "bot-info",
@@ -21,10 +24,11 @@ module.exports = {
             .setAuthor({ name: `Macaron Bleu`, iconURL: `https://media.discordapp.net/attachments/1267138684994584577/1270528074257006612/Default_Create_a_vibrant_twodimensional_cartoon_illustration_o_2.jpg?ex=66b406fa&is=66b2b57a&hm=d8c79811e51327eb265b8ed8441b2317fff6ce984e1dc79533a8489d29bf82e9&=&format=webp&width=701&height=701` })
             .setFields(
                 { name: `> <:discotoolsxyzicon83:1267636538179715142> Informations`, value: `${seta} Name: <@1266420565447544862> (\`${client.user.tag}\`)\n${seta} ID: ${client.user.id}\n${seta} Created: ${time} (${timeComplemento})\n${seta} Prefix: \`;\`\n${seta} Total commands: ${prefix + slash}` },
-                { name: `> <:statics:1270536665676382389> Stastistics`, value: `${seta} Total servers: **${client.guilds.cache.size}**\n${seta} Total users: ${users}` }
+                { name: `> <:statics:1270536665676382389> Stastistics`, value: `${seta} Total servers: **${client.guilds.cache.size}**\n${seta} Total users: ${users}\n${seta} Bot's ping: ${botPing}ms\n ${seta} Message's ping: ${messagePing}ms\n${seta} Database ping: ${databasePing}` },
+                { name: `> <:discotoolsxyzicon28:1267637289836871740> Versions:`, value: `${seta} Discord.js: ${discordjsversion}\n${seta} Node: ${node}` }
             )
             .setFooter({ text: `Thank you for choosing me S2`, iconURL: message.author.displayAvatarURL() })
 
-        message.reply({ embeds: [embedBotInfo] })
+        message.reply({ embeds: [embedBotInfo], components: [buttons] })
     }
 }
